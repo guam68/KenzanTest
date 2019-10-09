f57446f7de48b02b6625a9a7a23665dc8e8d4dcb
Daniel Bautista <danielbautista@Administrators-MacBook-Pro.local>
2019-10-09 10:12:56 -0600

    Initial commit - JPA setup, employee entity, entity test

---
+++ db/Kenzan.sql
@@ -0,0 +1,55 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema employeedb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `employeedb` ;
+
+-- -----------------------------------------------------
+-- Schema employeedb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `employeedb` DEFAULT CHARACTER SET utf8 ;
+USE `employeedb` ;
+
+-- -----------------------------------------------------
+-- Table `employee`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `employee` ;
+
+CREATE TABLE IF NOT EXISTS `employee` (
+  `id` INT NOT NULL,
+  `first_name` VARCHAR(45) NOT NULL,
+  `middle_initial` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NOT NULL,
+  `date_of_birth` DATE NULL,
+  `date_of_employment` DATE NULL,
+  `status` TINYINT NULL DEFAULT 1,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS employee;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'employee' IDENTIFIED BY 'employee';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'employee';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `employee`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `employeedb`;
+INSERT INTO `employee` (`id`, `first_name`, `middle_initial`, `last_name`, `date_of_birth`, `date_of_employment`, `status`) VALUES (1, 'James', 'E', 'Taylor', '1984-10-12', '2015-01-30', 1);
+INSERT INTO `employee` (`id`, `first_name`, `middle_initial`, `last_name`, `date_of_birth`, `date_of_employment`, `status`) VALUES (2, 'Sarah', NULL, 'Conner', '1967-04-24', '2018-04-13', 1);
+INSERT INTO `employee` (`id`, `first_name`, `middle_initial`, `last_name`, `date_of_birth`, `date_of_employment`, `status`) VALUES (3, 'Paul', 'R', 'West', '1990-11-02', '2019-06-12', 0);
+
+COMMIT;
+
