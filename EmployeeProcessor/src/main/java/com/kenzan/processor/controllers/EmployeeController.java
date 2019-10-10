f480e97d1d0c4804d03d9864901b47b9f0d85378
Daniel Bautista <danielbautista@Administrators-MacBook-Pro.local>
2019-10-09 22:46:25 -0600

    Add - POST addEmployee()

--- EmployeeProcessor/src/main/java/com/kenzan/processor/controllers/EmployeeController.java
+++ EmployeeProcessor/src/main/java/com/kenzan/processor/controllers/EmployeeController.java
@@ -2,11 +2,14 @@ package com.kenzan.processor.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +44,25 @@ public class EmployeeController {
 		}
 
 		return employee;
+	}
+	
+	@PostMapping("employees")
+	public Employee addEmployee(@RequestBody Employee employee, HttpServletResponse resp, 
+								HttpServletRequest req) {
+		Employee newEmp = null;
+
+		try {
+			newEmp = serv.createEmployee(employee);
+			resp.setStatus(200);
+			StringBuffer url = req.getRequestURL();
+			url.append("/" + newEmp.getId());
+			resp.setHeader("Location", url.toString());
+
+		} catch (Exception e) {
+			System.err.println(e);
+			resp.setStatus(400);
+		}
+		return newEmp;
 	}
 }
 
