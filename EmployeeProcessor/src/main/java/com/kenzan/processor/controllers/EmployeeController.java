52840897f1a9416da873468b4074829f72721ec7
Daniel Bautista <danielbautista@Administrators-MacBook-Pro.local>
2019-10-09 23:28:45 -0600

    Add - PUT updateEmployee()

--- EmployeeProcessor/src/main/java/com/kenzan/processor/controllers/EmployeeController.java
+++ EmployeeProcessor/src/main/java/com/kenzan/processor/controllers/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,6 +65,25 @@ public class EmployeeController {
 			resp.setStatus(400);
 		}
 		return newEmp;
+	}
+	
+	@PutMapping("employees/{id}")
+	public Employee updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee,
+									HttpServletResponse resp) {
+		Employee updated = null;
+		try {
+			updated = serv.updateEmployee(id, employee);
+			if (updated != null) {
+				resp.setStatus(200);
+			} else {
+				resp.setStatus(404);
+			}
+			
+		} catch (Exception e) {
+			System.err.println(e);
+			resp.setStatus(400);
+		}
+		return updated;
 	}
 	
 	@DeleteMapping("employees/{id}")
