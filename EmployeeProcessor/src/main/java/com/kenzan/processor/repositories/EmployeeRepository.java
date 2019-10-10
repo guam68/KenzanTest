6d3ebbcaee1d3e5c2cf371c8ac21b442dcc5edb8
Daniel Bautista <danielbautista@Administrators-MacBook-Pro.local>
2019-10-09 23:17:53 -0600

    Fix - GET methods return disabled employees

--- EmployeeProcessor/src/main/java/com/kenzan/processor/repositories/EmployeeRepository.java
+++ EmployeeProcessor/src/main/java/com/kenzan/processor/repositories/EmployeeRepository.java
@@ -1,7 +1,17 @@
 package com.kenzan.processor.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.kenzan.processor.entities.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	@Query("SELECT emp FROM Employee emp WHERE emp.status = true")
+	List<Employee> queryForActiveEmployees();
+	
+	@Query("SELECT emp FROM Employee emp WHERE emp.status = true AND emp.id = :id")
+	Employee queryForActiveEmployeeById(@Param("id") int id);
 }
